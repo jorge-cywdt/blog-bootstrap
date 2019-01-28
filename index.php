3455be04de5af309b9aa2830d09c655694ac2d02
Jorge <jorge.cywdt@gmail.com>
2019-01-28 13:38:32 -0500

    aaa

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <!-- Navbar -->
     <div class="container-fluid bg-light fixed-top"> <!-- container-fluid bg-primary fixed-top -->
         <nav class="navbar navbar-expand-lg navbar-light bg-light"> <!-- navbar navbar-expand-lg navbar-dark bg-primary -->
-            <a class="navbar-brand" href="index.html">Bootstrap</a>
+            <a class="navbar-brand" href="/">Bootstrap</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
