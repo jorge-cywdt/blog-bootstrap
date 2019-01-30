031078cf1c5c3b591da9af79a5d705ebd34d2b58
Jorge <jorge.cywdt@gmail.com>
2019-01-29 22:34:12 -0500

    aaa

--- index.php
+++ index.php
@@ -17,29 +17,7 @@
 <body>
     <!-- Navbar -->
     <div class="container-fluid bg-dark fixed-top"> <!-- container-fluid bg-primary fixed-top -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light d-none"> <!-- navbar navbar-expand-lg navbar-dark bg-primary -->
-            <a class="navbar-brand" href="/">Bootstrap</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="cover.php">Cover<span class="sr-only">(current)</span></a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <a class="btn btn-outline-dark my-2 my-sm-0" href="cover.php?action=contactame">Contacto</a> <!-- btn btn-outline-dark my-2 my-sm-0 text-white border border-white -->
-                    <!--  href="javascript:contacto()", es mejor pasar directamente la variable por href ya que es una y es estática, si se trata de muchas variables se realiza con una funcion javascript -->
-                </form>
-            </div>
-        </nav>
-
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> <!-- navbar navbar-expand-lg navbar-dark bg-primary -->
             <a class="navbar-brand" href="/">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
